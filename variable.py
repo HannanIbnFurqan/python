9106f05b174e8c39e951504bd65341556af70b00
Mohd Hannan <mohdhannan7913@gmail.com>
2025-02-11 11:59:19 +0530

    what is variable

---
+++ variable.py
@@ -0,0 +1,22 @@
+'''
+1.what is variable ?
+variable is like a container that holds data, very similar to how our containers in kitchen holds sugar, salt etc.
+'''
+
+# a is variable
+
+# addition
+a = 9+4
+print(a)
+
+# multiplication
+
+x = 5
+y = 5
+
+multi = x*y
+
+print(multi)
+
+
+
