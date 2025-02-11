b83954442d0631798b7fe55eba11975a9fef4318
Mohd Hannan <mohdhannan7913@gmail.com>
2025-02-11 17:20:27 +0530

    fundamental of dictionary

---
+++ Dictionary.py
@@ -0,0 +1,13 @@
+'''
+What is a Dictionary in Python?
+
+A dictionary in Python is an unordered, mutable (changeable) collection that stores data in key-value pairs.
+Unlike lists and tuples (which use indexing), dictionaries use unique keys to access values.
+'''
+
+# Creating a dictionary
+student = {
+    "name": "Hannan",
+    "age": 22,
+    "course": "Full Stack Development"
+}
